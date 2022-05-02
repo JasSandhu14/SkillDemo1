e34b17db874f06ddb815deab74ffec57f012564e
JasSandhu14 <103288252+JasSandhu14@users.noreply.github.com>
2022-05-01 22:44:48 -0700

    initial commit

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,8 @@
+public class SkillDemo{
 
+    //get two int values and return the count between them
+    public static int getCount(int a, int b){
+        return b - a;
+    }
+
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void getCountTest(){
+        assertEquals(15, SkillDemo.getCount(1,15)); //fail
+    }
+}
